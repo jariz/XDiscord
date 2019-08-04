2425819664c639616c538e0dacb281788b35b4db
Jari Zwarts <jari@oberon.nl>
2019-08-04 22:57:49 +0200

    Initial Commit

---
+++ XcodeRPC/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XcodeRPC
+//
+//  Created by Jari on 04/08/2019.
+//  Copyright © 2019 JARI.IO. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
